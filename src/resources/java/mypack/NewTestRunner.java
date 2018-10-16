06f1ff2efbd40ef5b8b214ca475302464857aafb
teamlead11 <automationlead11@gmail.com>
2018-10-16 11:27:54 +0530

    demo

--- src/resources/java/mypack/NewTestRunner.java
+++ src/resources/java/mypack/NewTestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = "src/resources/java/mypack/AddCustomer.feature", monochrome = true, dryRun = false, strict = true, glue = {
-		"mypack" }, plugin = { "json:target/cucumbReport.json" })
+		"mypack" }, plugin = { "json:src/resources/java/cucumbReport.json" })
 
 public class NewTestRunner {
 
